bf0897fa8087b1df339a1c9c31589a45554656a4
froeh010 <froeh010@uni-hildesheim.de>
2020-09-18 13:52:15 +0200

    Initial commit after moving from simplic-service

---
+++ src/Simplic.Session/Interface/ISessionService.cs
@@ -0,0 +1,20 @@
+﻿namespace Simplic.Session
+{
+    /// <summary>
+    /// Tenant changed delegate
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="args">Args</param>
+    public delegate void OrganizationSelectionChangedEventHandler(object sender, SelectedOrganizationsChangedArgs args);
+
+    /// <summary>
+    /// Session service holds a variable to the current session
+    /// </summary>
+    public interface ISessionService
+    {
+        /// <summary>
+        /// Gets or sets the current session
+        /// </summary>
+        Session CurrentSession { get; set; }
+    }
+}
